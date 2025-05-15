73fd5c6c01a051498c697eca8584c248cebd2613
MarcoLu <154767894+MarcoLu922@users.noreply.github.com>
2025-05-15 16:36:46 +0800

    更新备注

--- hello/hello.go
+++ hello/hello.go
@@ -5,10 +5,10 @@ func main() {
 	fmt.Println("Hello, World!")
 }
 
-// go run hello.go
+// go run hello.go -> run the program
 
-// go build hello.go
-// ./hello
+// go build hello.go -> build the program
+// ./hello -> run the program
 
-// go mod init foo/bar
-// go mod tidy
+// go mod init foo/bar -> create a new module
+// go mod tidy -> add missing and remove unused modules
